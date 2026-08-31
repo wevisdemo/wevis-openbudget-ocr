965c8b8aa7f53d5d4d33c95ef3857b4b4a16a384
patiphan2000 <srichai.patiphan@gmail.com>
2026-08-31 17:24:27 +0700

    fix(scrape-budget-pdf): remove headless

--- script/scrape_and_download_pdf.py
+++ script/scrape_and_download_pdf.py
@@ -18,7 +18,7 @@ from selenium.webdriver.support import expected_conditions as EC
 def setup_driver():
     """Configures and returns a Selenium WebDriver instance."""
     chrome_options = Options()
-    chrome_options.add_argument("--headless")
+    # chrome_options.add_argument("--headless")
     driver = webdriver.Chrome(options=chrome_options)
     return driver
 
