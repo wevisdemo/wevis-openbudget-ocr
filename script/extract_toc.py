48710f2fd1a2c56b7d810d5ef577fe922cd7673b
patiphan2000 <srichai.patiphan@gmail.com>
2026-08-31 17:27:16 +0700

    feat: split toc into separate file

---
+++ script/extract_toc.py
@@ -0,0 +1,22 @@
+import os
+import argparse
+from toc_extractor import extract_pdf_toc_to_json
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--pdf_dir", default="example/pdf", help="Path to directory contains pdf files")
+    parser.add_argument("--toc_out", default="output/toc", help="Output path for extracted Table of Content (json)")
+    
+    args = parser.parse_args()
+    
+    PDF_DIR_PATH = args.pdf_dir
+    TOC_OUT_PATH = args.toc_dir
+    
+    # Check and create output path
+    os.makedirs(TOC_OUT_PATH, exist_ok=True)
+    
+    # Extract TOC
+    extract_pdf_toc_to_json(PDF_DIR_PATH, TOC_OUT_PATH)
+    
+if __name__ == "__main__":
+    main()
--- script/ocr_budget_pdf.py
+++ script/ocr_budget_pdf.py
@@ -1,13 +1,12 @@
 import os
 import json
 import argparse
-from toc_extractor import extract_pdf_toc_to_json
 from thai_budget_extractor import extract_budget_object
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--pdf_dir", default="example/pdf", help="Path to directory contains pdf files")
-    parser.add_argument("--toc_out", default="output/toc", help="Output path for extracted Table of Content (json)")
+    parser.add_argument("--toc_dir", default="output/toc", help="Output path for extracted Table of Content (json)")
     parser.add_argument("--tree_out", default="output/tree", help="Output path for extracted Budget Tree (csv)")
     parser.add_argument("--obj_out", default="output/obj", help="Output path for extracted Budget Object (json)")
     
@@ -18,21 +17,18 @@ def main():
     args = parser.parse_args()
     
     PDF_DIR_PATH = args.pdf_dir
-    TOC_OUT_PATH = args.toc_out
+    TOC_DIR_PATH = args.toc_dir
     TREE_OUTPATH = args.tree_out
     OBJ_OUTPATH = args.obj_out
     SKIP_EXISTING = args.skip_existing
     
     # Check and create output path
-    os.makedirs(TOC_OUT_PATH, exist_ok=True)
+    os.makedirs(TOC_DIR_PATH, exist_ok=True)
     os.makedirs(TREE_OUTPATH, exist_ok=True)
     os.makedirs(OBJ_OUTPATH, exist_ok=True)
     
-    # Extract TOC data index
-    extract_pdf_toc_to_json(PDF_DIR_PATH, TOC_OUT_PATH)
-    
     # Extract budget data as object
-    toc_files_list = sorted(os.listdir(TOC_OUT_PATH))
+    toc_files_list = sorted(os.listdir(TOC_DIR_PATH))
     for ministry_toc in toc_files_list:
         if not ministry_toc.endswith('.json'): continue
         ministry_name = ministry_toc.split(".")[0]
@@ -45,7 +41,7 @@ def main():
             print(f"Skipping {ministry_name} (already exists)")
             continue
         
-        with open(os.path.join(TOC_OUT_PATH, ministry_toc), "r") as f:
+        with open(os.path.join(TOC_DIR_PATH, ministry_toc), "r") as f:
             toc_data = json.load(f)
         ministry = extract_budget_object(toc_data, pdf_dir=PDF_DIR_PATH)
         
